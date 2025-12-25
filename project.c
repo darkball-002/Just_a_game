5380ceee22731228c43ad956dc4e665f28dfbf56
darkball-002 <pp100qq20@gmail.com>
2025-12-25 10:51:57 +0800

    I just fix my syntax error

--- project.c
+++ project.c
@@ -5,7 +5,8 @@ const char* state = "beginning";
 
 const char* why = "I just try to do something for my self";
 
-const char* about_task = " \n
-  I use neovim that always running in the root directory(the \"project.c\" exist place). \n
-  if you want use those batch files, must call it on the root directory(for the stupid working directory). \n
-";
+const char* about_task =
+  "I use neovim that always running in the root directory(the \"project.c\" exist place). \n"
+  "if you want use those batch files, must call it on the root directory(for the stupid working directory)."
+;
+
