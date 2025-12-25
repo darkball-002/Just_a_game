f13473ded88174ff840d5066a709b23adaac0150
darkball-002 <pp100qq20@gmail.com>
2025-12-25 10:54:17 +0800

    why I lost the freaking "\n"

--- project.c
+++ project.c
@@ -7,6 +7,7 @@ const char* why = "I just try to do something for my self";
 
 const char* about_task =
   "I use neovim that always running in the root directory(the \"project.c\" exist place). \n"
-  "if you want use those batch files, must call it on the root directory(for the stupid working directory)."
+  "if you want use those batch files, must call it on the root directory(for the stupid working directory). \n"
 ;
+
 
